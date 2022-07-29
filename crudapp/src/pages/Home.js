15ba20324c296aeb3a1625ab1b01f5797af8b57a
brian ambundo <brian.katiechi9@gmail.com>
2022-07-29 11:20:40 +0300

    fetched the data using axios and mapped it onto the Homepage

--- crudapp/src/pages/Home.js
+++ crudapp/src/pages/Home.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3003/users")
+            .then((res) => {
+                setUsers(res.data)
+            })
+    }, [])
+
     return (
         <div className='w-full h-full flex flex-col px-10 py-8'>
             <h1 className='text-black text-3xl font-semibold font-Monteserrat'>Home Page</h1>
@@ -30,30 +41,35 @@ function Home() {
 
                                     </tr>
                                 </thead>
+
                                 <tbody>
-                                    <tr className="bg-white border-b">
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">1</td>
-                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            Mark
-                                        </td>
-                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            Otto
-                                        </td>
-                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            @mdo
-                                        </td>
-                                        <td className="flex justify-center items-center text-sm flex space-x-4 mt-1">
-                                            <button className='px-6 py-2 text-white bg-black rounded-lg'>
-                                                View
-                                            </button>
-                                            <button className='px-6 py-2 text-white bg-blue-600 rounded-lg'>
-                                                Edit
-                                            </button>
-                                            <button className='px-6 py-2 text-white bg-red-600 rounded-lg'>
-                                                Delete
-                                            </button>
-                                        </td>
-                                    </tr>
+
+                                    {users.map((user, index) => {
+                                        return (<tr key={index} className="bg-white border-b">
+                                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{index+1}</td>
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                {user.name}
+                                            </td>
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                {user.email}
+                                            </td>
+                                            <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                                {user.phone}
+                                            </td>
+                                            <td className="flex justify-center items-center text-sm flex space-x-4 mt-1">
+                                                <button className='px-6 py-2 text-white bg-black rounded-lg'>
+                                                    View
+                                                </button>
+                                                <button className='px-6 py-2 text-white bg-blue-600 rounded-lg'>
+                                                    Edit
+                                                </button>
+                                                <button className='px-6 py-2 text-white bg-red-600 rounded-lg'>
+                                                    Delete
+                                                </button>
+                                            </td>
+                                        </tr>)
+                                    })}
+
 
                                 </tbody>
                             </table>
