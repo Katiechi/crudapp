2b0b5ddf8453168b6738561d05c9b1bdfb3f63dc
brian ambundo <brian.katiechi9@gmail.com>
2022-08-01 06:46:06 +0300

    implemented the delete functionality

--- crudapp/src/pages/Home.js
+++ crudapp/src/pages/Home.js
@@ -6,12 +6,26 @@ function Home() {
 
     const [users, setUsers] = useState([])
 
-    useEffect(() => {
+    const loadUsers = ()=>{
+
+        
         axios.get("http://localhost:3003/users")
             .then((res) => {
                 setUsers(res.data.reverse())
             })
+        }
+
+    useEffect(() => {
+     loadUsers()
     }, [])
+
+
+    function Delete(id){
+        axios.delete(`http://localhost:3003/users/${id}`)
+        .then(
+            loadUsers()
+        )
+    }
 
     return (
         <div className='w-full h-full flex flex-col px-10 py-8'>
@@ -45,26 +59,26 @@ function Home() {
 
                                 <tbody>
 
-                                    {users.map((user, index) => {
+                                    {users.map((data, index) => {
                                         return (<tr key={index} className="bg-white border-b">
                                             <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{index+1}</td>
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                {user.name}
+                                                {data.name}
                                             </td>
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                {user.email}
+                                                {data.email}
                                             </td>
                                             <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                                {user.phone}
+                                                {data.phone}
                                             </td>
                                             <td className="flex justify-center items-center text-sm flex space-x-4 mt-1">
-                                                <Link to={`/users/${user.id}`} className='px-6 py-2 text-white bg-black rounded-lg'>
+                                                <Link to={`/users/${data.id}`} className='px-6 py-2 text-white bg-black rounded-lg'>
                                                     View
                                                 </Link>
-                                                <button className='px-6 py-2 text-white bg-blue-600 rounded-lg'>
+                                                <button  className='px-6 py-2 text-white bg-blue-600 rounded-lg'>
                                                     Edit
                                                 </button>
-                                                <button className='px-6 py-2 text-white bg-red-600 rounded-lg'>
+                                                <button onClick={()=>Delete(data.id)} className='px-6 py-2 text-white bg-red-600 rounded-lg'>
                                                     Delete
                                                 </button>
                                             </td>
